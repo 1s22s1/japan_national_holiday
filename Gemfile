c3a5c79d3c5ff4cf0e034c38f5d523880a25f884
1s22s1 <1s22s1@example.com>
2023-09-20 06:21:26 +0900

    First commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in japan_national_holiday.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/japan_national_holiday.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "japan_national_holiday/version"
+
+module JapanNationalHoliday
+  class Error < StandardError; end
+  # Your code goes here...
+end
