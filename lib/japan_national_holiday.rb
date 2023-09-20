929d5b212e2e4f89c91044e8c30d774de430fc18
1s22s1 <1s22s1@example.com>
2023-09-21 08:03:50 +0900

    wip

--- lib/japan_national_holiday.rb
+++ lib/japan_national_holiday.rb
@@ -7,7 +7,7 @@ require_relative 'japan_national_holiday/version'
 
 module JapanNationalHoliday
   def self.holiday?(target_date)
-    holidays = YAML.load('japan_national_holiday/holidays.yml')
+    holidays = open('holidays.yml', 'r') { |f| YAML.unsafe_load_file(f) }
 
     true
   end
